1f540457004f4b9280a23dcb6c21bd960d904a98
Alexey <alexey.ignatenko@gmail.com>
2021-07-30 01:13:49 +0300

    Removed MFC usage

--- AltFS/Src/AltFS.cpp
+++ AltFS/Src/AltFS.cpp
@@ -1,164 +1,172 @@
 
 // AltFS.cpp : Defines the class behaviors for the application.
 //
-
 #include "stdafx.h"
-#include "AltFS.h"
-#include "AltFSDlg.h"
+
 #include "FSUIPCEngine.h"
 
-
-#include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/spdlog.h>
 
 #include <iostream>
 
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#endif
+#define WIN32_LEAN_AND_MEAN  // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 
-#define WM_CUSTOM_MESSAGE WM_USER +1
+constexpr int WM_CUSTOM_MESSAGE = WM_USER + 1;
 
-// CAltFSApp
+// register our message
+static const UINT XC_CALL = RegisterWindowMessage("FsasmLib:IPC");
 
-BEGIN_MESSAGE_MAP(CAltFSApp, CWinApp)
-	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
-     ON_THREAD_MESSAGE(WM_CUSTOM_MESSAGE, OnCustomMessage)
-END_MESSAGE_MAP()
-
-
-// CAltFSApp construction
-
-CAltFSApp::CAltFSApp()
-{
-}
-
-
-// The one and only CAltFSApp object
-
-CAltFSApp theApp;
-
+FSUIPCEngine* s_engine = nullptr;
 
 class MFCRunner : public Runner
 {
 public:
-
     MFCRunner()
     {
+        m_threadId = ::GetCurrentThreadId();
         Runner::threadInstance = this;
     }
 
-    ~MFCRunner()
-    {
-        Runner::threadInstance = nullptr;
-    }
+    ~MFCRunner() { Runner::threadInstance = nullptr; }
 
-    virtual bool run(std::function<void()> f)  override
+    virtual bool run(std::function<void()> f) override
     {
         std::function<void()>* func = new std::function<void()>(f);
-        AfxGetApp()->PostThreadMessage(WM_USER + 1, WPARAM(func), 0);
+        PostThreadMessage(m_threadId, WM_USER + 1, WPARAM(func), 0);
         return true;
     }
+
+private:
+    DWORD m_threadId;
 };
 
-BOOL CAltFSApp::InitInstance()
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-      if (AllocConsole())
-	{
-		FILE *fpstdin = stdin, *fpstdout = stdout, *fpstderr = stderr;
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
 
-		freopen_s(&fpstdin, "CONIN$", "r", stdin);
-		freopen_s(&fpstdout, "CONOUT$", "w", stdout);
-		freopen_s(&fpstderr, "CONOUT$", "w", stderr);
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-		std::cout << "This is a test of the attached console" << std::endl;
-		//FreeConsole();
-	}
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    case WM_CUSTOM_MESSAGE:
+    {
+        auto* func = reinterpret_cast<std::function<void()>*>(wParam);
+        (*func)();
+        delete func;
+        return 0;
+    }
+    }
+
+    if (uMsg == XC_CALL)
+    {
+        return s_engine->processMessage(wParam, lParam);
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    if (AllocConsole())
+    {
+        FILE *fpstdin = stdin, *fpstdout = stdout, *fpstderr = stderr;
+
+        freopen_s(&fpstdin, "CONIN$", "r", stdin);
+        freopen_s(&fpstdout, "CONOUT$", "w", stdout);
+        freopen_s(&fpstderr, "CONOUT$", "w", stderr);
+
+        std::cout << "This is a test of the attached console" << std::endl;
+        // FreeConsole();
+    }
 
     // get filename of the executable
-    CString path;
-    GetModuleFileName(NULL, path.GetBuffer(MAX_PATH), MAX_PATH);
-    path.ReleaseBuffer();
+    char path[MAX_PATH + 1];
+    GetModuleFileName(NULL, path, MAX_PATH);
 
     std::filesystem::path exeName = static_cast<LPCSTR>(path);
     std::filesystem::path exePath = exeName.parent_path();
 
     // Set the default logger to file logger
-    spdlog::set_level(spdlog::level::info); // Set global log level to debug
-    
-    //auto logFilename = exePath / "altfs.log";
-    //auto file_logger = spdlog::basic_logger_mt("basic_logger", logFilename.string());
-    //spdlog::set_default_logger(file_logger);
-    auto console = spdlog::stdout_color_mt("console");   
+    spdlog::set_level(spdlog::level::info);  // Set global log level to debug
+
+    // auto logFilename = exePath / "altfs.log";
+    // auto file_logger = spdlog::basic_logger_mt("basic_logger", logFilename.string());
+    // spdlog::set_default_logger(file_logger);
+    auto console = spdlog::stdout_color_mt("console");
     spdlog::set_default_logger(console);
 
     spdlog::info("AltFs started");
 
-    // InitCommonControlsEx() is required on Windows XP if an application
-	// manifest specifies use of ComCtl32.dll version 6 or later to enable
-	// visual styles.  Otherwise, any window creation will fail.
-	INITCOMMONCONTROLSEX InitCtrls;
-	InitCtrls.dwSize = sizeof(InitCtrls);
-	// Set this to include all the common control classes you want to use
-	// in your application.
-	InitCtrls.dwICC = ICC_WIN95_CLASSES;
-	InitCommonControlsEx(&InitCtrls);
+    // Register the window class.
+    const char CLASS_NAME[] = "FS98MAIN";
 
-	CWinApp::InitInstance();
+    WNDCLASS wc = {};
 
-    // register FS98MAIN as classname for the dialog
-    WNDCLASS wc;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
 
-  	// Get the info for this class.
-           // #32770 is the default class name for dialogs boxes.
-  	::GetClassInfo(AfxGetInstanceHandle(), "#32770", &wc);
+    RegisterClass(&wc);
 
-  	// Change the name of the class.
-  	wc.lpszClassName = "FS98MAIN";
+    // Create the window.
 
-  	// Register this class so that MFC can use it.
-  	AfxRegisterClass(&wc);
+    HWND hwnd = CreateWindowEx(0,                    // Optional window styles.
+                               CLASS_NAME,           // Window class
+                               "XPWideClient",       // Window text
+                               WS_OVERLAPPEDWINDOW,  // Window style
 
-  
+                               // Size and position
+                               CW_USEDEFAULT, CW_USEDEFAULT, 300, 200,
 
-    // Standard initialization
-    SetRegistryKey(_T("Alexey Ignatenko"));
+                               NULL,       // Parent window
+                               NULL,       // Menu
+                               hInstance,  // Instance handle
+                               NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
 
     MFCRunner runner;
     FSUIPCEngine engine((exePath / "lua").string());
-   
-    CAltFSDlg dlg(engine);
-    m_pMainWnd = &dlg;
+    s_engine = &engine;
 
-    engine.init()
-        .fail([&dlg](std::string e)
+    engine.init().fail(
+        [](const std::string& e)
         {
-            ::MessageBox(NULL, CString("Fatal error: ") + e.c_str(), "Error", MB_OK);
-            dlg.EndDialog(-1);
+            ::MessageBox(NULL, fmt::format("Fatal error: {}", e).c_str(), "Error", MB_OK);
+            PostQuitMessage(1);
         });
 
-    INT_PTR nResponse = dlg.DoModal();
-    if (nResponse == -1)
+    // Run the message loop.
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
     {
-        TRACE(traceAppMsg, 0, "Warning: dialog creation failed, so application is terminating unexpectedly.\n");
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-#if !defined(_AFXDLL) && !defined(_AFX_NO_MFC_CONTROLS_IN_DIALOGS)
-    ControlBarCleanUp();
-#endif
+    s_engine = nullptr;
 
-
-    // Since the dialog has been closed, return FALSE so that we exit the
-    //  application, rather than start the application's message pump.
-    return FALSE;
+    return 0;
 }
-
-inline void CAltFSApp::OnCustomMessage(WPARAM wParam, LPARAM lParam)
-{
-    auto* func = reinterpret_cast<std::function<void()>*>(wParam);
-    (*func)();
-    delete func;
-}
-
--- AltFS/Src/FSUIPCEngine.h
+++ AltFS/Src/FSUIPCEngine.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <filesystem>
 #include <map>
 #include <memory>
-#include <filesystem>
 
 #include <LuaEngine/LuaEngine.h>
-#include <LuaEngine/LuaXPlane.h>
 #include <LuaEngine/LuaLogging.h>
+#include <LuaEngine/LuaXPlane.h>
+
+#define WIN32_LEAN_AND_MEAN  // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 
 class FSUIPCEngine
 {
@@ -19,17 +22,13 @@ public:
     LRESULT processMessage(WPARAM wParam, LPARAM lParam);
 
 private:
-
     void readFromSim(DWORD offset, DWORD size, void* data);
     promise::Defer writeToSim(DWORD offset, DWORD size, const void* data);
 
-  
 private:
-
     std::map<ATOM, std::pair<HANDLE, BYTE*>> m_fileMap;
 
-    LuaEngine m_lua; 
+    LuaEngine m_lua;
     LuaXPlane m_xPlaneModule;
     LuaLogging m_logModule;
-    
 };
