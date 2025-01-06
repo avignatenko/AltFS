835b05f36651ee92768fcf08432ff58955e25e70
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2025-01-07 01:14:30 +0300

    added simple test

---
+++ XPlaneUDPClientCpp/Tests/BeaconListenerTests.cpp
@@ -0,0 +1,25 @@
+#include <XPlaneUDPClientCpp/BeaconListener.h>
+#include <catch2/catch_test_macros.hpp>
+
+#include <asio/io_context.hpp>
+
+TEST_CASE("Basic discovery test", "[BeaconListener]")
+{
+    asio::io_context io;
+    auto work = asio::make_work_guard(io);
+
+    xplaneudpcpp::BeaconListener listener;
+    listener.getXPlaneServerBroadcast(io.get_executor())
+        .then(
+            [&work](xplaneudpcpp::BeaconListener::ServerInfo info)
+            {
+                REQUIRE(info.hostId == 1);
+                REQUIRE(info.version == 1);
+                REQUIRE(info.host == "192.168.0.01");
+                REQUIRE(info.port == 49000);
+
+                work.reset();
+            });
+
+    io.run();
+}
