4324f948adde03ac10df289a15584deaab398689
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2025-01-09 15:05:40 +0300

    better async

--- LuaEngine/Include/LuaEngine/LuaEngine.h
+++ LuaEngine/Include/LuaEngine/LuaEngine.h
@@ -2,6 +2,10 @@
 
 #include "LuaModule.h"
 
+#include <XPlaneUDPClientCpp/ActiveObject.h>
+
+#include <asio/executor.hpp>
+
 #include <filesystem>
 
 namespace sol
@@ -11,18 +15,11 @@ class state;
 
 class OffsetStatsGenerator;
 
-class LuaEngine : public LuaModuleAPI
+class LuaEngine
 {
 public:
     LuaEngine(const std::filesystem::path& scriptPath);
     ~LuaEngine();
-
-    // lua module api
-
-    cti::continuable<> runAsync(std::function<void(sol::state&)> f) override
-    {
-        return cti::async([this, f] { f(lua()); });
-    }
 
     // own
     void load();
@@ -30,9 +27,9 @@ public:
     void readFromSim(uint32_t offset, uint32_t size, std::byte* data);
     void writeToSim(uint32_t offset, uint32_t size, const std::byte* data);
 
-private:
     sol::state& lua() { return *m_lua; }
 
+private:
     enum class FSUIPCType
     {
         UInt8 = 1,
@@ -53,4 +50,43 @@ private:
     std::filesystem::path m_scriptPath;
     std::unique_ptr<OffsetStatsGenerator> m_stats;
     bool m_initialized = false;
+};
+
+class LuaEngineAsync : public LuaModuleAPI, private ActiveObject
+{
+public:
+    LuaEngineAsync(const std::filesystem::path& scriptPath, asio::any_io_executor caller) : ActiveObject(caller)
+    {
+        ActiveObject::runAsync([this, scriptPath] { engine_ = std::make_unique<LuaEngine>(scriptPath); });
+    }
+
+    ~LuaEngineAsync()
+    {
+        ActiveObject::runAsync([this] { engine_.reset(); });
+    }
+
+    cti::continuable<> load()
+    {
+        return ActiveObject::runAsync([this] { engine_->load(); });
+    }
+
+    cti::continuable<> readFromSim(uint32_t offset, uint32_t size, std::byte* data)
+    {
+        return ActiveObject::runAsync([this, offset, size, data] { engine_->readFromSim(offset, size, data); });
+    }
+    cti::continuable<> writeToSim(uint32_t offset, uint32_t size, const std::byte* data)
+    {
+        return ActiveObject::runAsync([this, offset, size, data] { engine_->writeToSim(offset, size, data); });
+    }
+
+private:
+    // lua module api
+
+    cti::continuable<> runAsync(std::function<void(sol::state&)> f) override
+    {
+        return cti::async_on([this, f] { f(engine_->lua()); }, asioPostExecutor(context().get_executor()));
+    }
+
+private:
+    std::unique_ptr<LuaEngine> engine_;
 };
